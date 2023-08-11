b1c2e021a80ece07c38330209ef41f2267775dcf
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-11 07:35:00 -0700

    nn

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: A C program that prints using sizeof
  *
- * Return 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
@@ -15,4 +15,5 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
