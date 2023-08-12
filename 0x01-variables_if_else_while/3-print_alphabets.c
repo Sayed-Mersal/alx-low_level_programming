ea0248f2893d9a77a7c7f10f7797a32cfe655698
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-11 17:24:45 -0700

    dd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alph = 'a';
+	char Alph = 'A';
+
+	while (alph <= 'a')
+	{
+		putchar(alph);
+		alph++;
+	}
+
+	while (Alph <= 'A')
+	{
+		putchar(Alph);
+		Alph++;
+	}
+	putchar('\n')
+
+	return (0);
+}
