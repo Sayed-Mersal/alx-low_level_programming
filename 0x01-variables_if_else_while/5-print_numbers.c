c70c811375ca643f1111d139bd3ef35d823371d1
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-11 18:00:38 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x % 2 != 0 && x <= 10)
+	{
+		printf(x);
+		x++;
+	}
+	printf('\n');
+	return (0);
+}
