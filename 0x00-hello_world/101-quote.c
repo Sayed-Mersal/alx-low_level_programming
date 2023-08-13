79bc83b62a839b007cd842f363573f289048cfa3
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-13 13:21:56 -0700

    dd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+*/
+
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, sizeof(text) - 1);
+
+	return (1);
+}
