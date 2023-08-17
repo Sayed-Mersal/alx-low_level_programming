e6eda8363d0174608cab95d3aa27e70e82928e26
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-16 18:13:45 -0700

    ss

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  jack_bauer- prints every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			-putchar((hr / 10) + 48);
+			-putchar((hr % 10) + 48);
+			-putchar(':');
+			-putchar((min / 10) + 48);
+			-putchar((min % 10) + 48);
+			-putchar('\n');
+		}
+	}
+}
