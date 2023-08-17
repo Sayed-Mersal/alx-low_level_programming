34ad2806a981227190c10dfe8c5a9448d5b978e4
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-16 17:28:58 -0700

    asd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function computes the abs of an integer
+ *
+ * @n: takes integer type input for function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abc(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
