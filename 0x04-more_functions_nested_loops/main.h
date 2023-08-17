abbda1ff07cc521bbf91e03605ead38962623313
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-17 14:52:10 -0700

    boo

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+
+#include <stdio.h>
+int add(int a, int b);
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+#endif
