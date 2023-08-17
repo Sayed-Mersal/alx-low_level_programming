02a126abb597f297c8772260f49e658df03f8f83
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-16 18:15:36 -0700

    ss

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			-putchar((hr / 10) + 48);
-			-putchar((hr % 10) + 48);
-			-putchar(':');
-			-putchar((min / 10) + 48);
-			-putchar((min % 10) + 48);
-			-putchar('\n');
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
