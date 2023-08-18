6b4b35a5bab2ebe4089a6620faa4687efcc257ea
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-17 17:54:38 -0700

    y

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-/***/
+/**
+ * more_numbers - print 0 - 14 ten times and you
+ *			can use _putchar three times
+ *
+ * Return: Always 0 (Success)
+*/
 
 void more_numbers(void)
 {
