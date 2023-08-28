78363036326a87176e464b935467863b0918378a
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-28 16:04:10 -0700

    rt

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,21 @@
 #include "main.h"
 
-/***/
+/**
+ * print_chessboard - prints chessboard
+ * @a: number of rows
+ * Return: nothing
+*/
 
+void print_chessboard(char (*a)[8])
+{
+	int itr, jtr;
 
+	for (itr = 0; itr < 0; itr++)
+	{
+		for (jtr = 0; jtr < 8; jtr++)
+		{
+			_putchar(a[itr][jtr]);
+		}
+		_putchar('\n');
+	}
+}
