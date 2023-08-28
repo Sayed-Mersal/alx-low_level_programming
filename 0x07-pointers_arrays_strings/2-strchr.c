d9abe06f55d658cb1e96254f3f479babb64d8e66
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-28 15:18:40 -0700

    dd

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,23 @@
 #include "main.h"
 
-/***/
+/**
+ * *_strchar - fils memory with a constant byte
+ * @s: pointer to put the constant
+ *@c: constant
+ * Return: pointer to s
+*/
 
+char *_strchar(char *s, char c)
+{
+	int itr;
 
+	for (itr = 0; s[itr] >= '\0' ; itr++)
+	{
+		if (s[itr] == c)
+		{
+			return (s + itr);
+		}
+	}
+
+	return ('\0')
+}
