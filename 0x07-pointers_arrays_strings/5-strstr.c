d0e2a324d0571c668df26bd53db4d063e413b69f
Sayed-Mersal <sayedmersal63@gmail.com>
2023-08-28 19:59:50 -0700

    gg

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints chessboard
  * @a: number of rows
  * Return: nothing
 */
 
-void print_chessboard(char (*a)[8])
+char *_strstr(char *haystack, char *needle)
 {
-	int itr, jtr;
 
-	for (itr = 0; itr < 8; itr++)
-	{
-		for (jtr = 0; jtr < 8; jtr++)
-		{
-			_putchar(a[itr][jtr]);
-		}
-		_putchar('\n');
-	}
 }
